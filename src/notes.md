1e56b1d3d14fab66390813deaf7b46d8d3eae9fd
Brandon <2017BrandonH@gmail.com>
2025-04-01 22:18:19 -0400

    added content to the logger. still needs lots of work

--- src/notes.md
+++ src/notes.md
@@ -1,5 +1,6 @@
 # Engine notes and todos while on cruise
 - take a better look at how the window is reading and using the buffer. we should probably specify "UpperLeft" and fix the drawing of shapes accordingly.
 - when home refactor shapes to be its own module with smaller files.
-- get chrono crate for Local
-- look at uuid options
+- get chrono crate for Local -> chrono crate acquired just 
+- look at uuid options -> we are using just the uuid library
+- should look at providing a real time feed with options with the TEPIMsg type and a preformatted string tuple or just one or the other. This would be to help create a console app. 
--- src/util/debug.rs
+++ src/util/debug.rs
@@ -1,15 +1,19 @@
-use std::{collections::LinkedList, time::SystemTime};
+use std::collections::LinkedList;
+
+use chrono::{DateTime, Local, TimeZone};
+use uuid::Uuid;
 
 struct TEPILogger {
-    id: u32,
+    id: Uuid,
     buffer: LinkedList<TEPILogMsg>,
     name: String,
     file_location: String,
-    buffer_amount: u16
+    buffer_amount: u16,
+    date_started: DateTime<Local>
 }
 
 struct TEPILogMsg {
-    timestamp: SystemTime,
+    timestamp: DateTime<Local>,
     log_type: TEPILogType,
     filters: LinkedList<String>,
     message: String,
@@ -29,12 +33,14 @@ impl TEPILogger {
         // for now we will write to a txt file but will do something more compressed
         let mut b: u16 = 16;
         if let Some(b) = buffer {};
+        let d = Local::now();
         TEPILogger {
-            id: 1,
+            id: Uuid::new_v4(),
             buffer: LinkedList::new(),
             name: name,
             file_location: fl,
-            buffer_amount: b
+            buffer_amount: b,
+            date_started: d
         }
     }
 
@@ -46,7 +52,7 @@ impl TEPILogger {
             }
         }
         self.buffer.push_back(TEPILogMsg {
-            timestamp: SystemTime::now(),
+            timestamp: Local::now(),
             log_type: log_type,
             filters: f,
             message: message,
@@ -109,15 +115,122 @@ impl TEPILogger {
 
         // write payload to associated file
     }
+
+    /// write_header
+    /// creates a header for use
+    /// Log: LogName
+    /// Date started:
+    /// Time started:
+    /// Log Version: 1
+    fn write_header(&self) -> String {
+        let date = self.date_started.format("%Y-%m-%d").to_string();
+        let time = self.date_started.format("%H:%M:%s");
+        let version: u8 = 1;
+        return format!("Log: {}\nDate Started: {}\nTime Started: {}\nLog Version: {}", self.name, date, time, version);
+
+    }
+
+    /// read_header
+    /// reads in a header.
+    /// we might need to make different header versions so we'll start accounting for this
+    /// in the read and write by using an integer for version.
+    /// until the initial log version is hammered down we will not increment versions.
+    fn read_header() {
+
+    }
 }
 
 impl TEPILogMsg {
-    pub fn fmt_log_string(&self) -> String{
-        let s = "timestamp";
+    /// fmt_log_string
+    /// creates a string usable for either writing to a file or a live feed.
+    /// example string without filters
+    /// [HH:MM:SS][TYPE] message
+    /// example string with filters
+    /// [HH:MM:SS][TYPE][filter1][filter2] message
+    pub fn fmt_log_string(&self) -> String {
+        let s = Local::now().format("%H:%M:%s");
         let log_type = TEPILogger::log_type_str(self.log_type, self.log_type_custom.clone());
-        let text = format!("\n[{s}][{log_type}]: {}", &self.message);
+        let filters = self.fmt_log_filters();
+        return format!("\n[{}][{}]{}: {}", s, log_type, filters, &self.message);
+    }
 
-        return s.to_string()
+    pub fn fmt_log_filters(&self) -> String {
+        let s = self.filters.iter().map(|i| i.to_owned()).collect::<Vec<String>>().join("]["); // there was a shorter way to achieve the same thing that I lost
+        return format!("[{}]", s);
+    }
+
+    pub fn read_msg(msg: String) -> TEPILogMsg {
+        let mut m = msg.clone();
+        let msg: &mut str = m.as_mut_str();
+
+        let mut words: Vec<String> = Vec::new();
+        let mut log_msg: String = String::new();
+        let mut inside_bracket: bool = false;
+        let mut curr_word: String = String::new();
+
+        // handle the line character by character.
+        for c in msg.chars() {
+            match c {
+                '[' => {
+                    // can probably get rid of this conditional since logs aren't really supposed to be tampered with
+                    if inside_bracket {
+                        if !curr_word.is_empty() {
+                            words.push(curr_word.clone());
+                            curr_word.clear();
+                        }
+                    }
+                    inside_bracket = true
+                }
+                ']' => {
+                    if inside_bracket {
+                        words.push(curr_word.clone());
+                        curr_word.clear();
+                        inside_bracket = false;
+                    } else {
+                        
+                    }
+                }
+                _ => {
+                    if inside_bracket {
+                        curr_word.push(c);
+                    } else {
+                        log_msg.push(c);
+                    }
+                }
+            };
+        }
+        // get our filters if they exist
+        let mut filters: Vec<String> = Vec::new();
+        while words.len() > 2 {
+            filters.push(words.pop().expect("missing line info"));
+        }
+        // we're now at the log type and the timestamp
+        let log_type_str = words.pop().expect("unable to find type info");
+        let log_type: TEPILogType = TEPILogMsg::match_log_type(log_type_str.clone());
+        let log_type_c: Option<String>;
+        let filters: LinkedList<String> = filters.into_iter().collect();
+        if log_type == TEPILogType::CUSTOM {
+            log_type_c = Some(log_type_str);
+        } else {
+            log_type_c = None
+        }
+        let time = words.pop().expect("unable to find time info");
+        return TEPILogMsg {
+            timestamp: DateTime::parse_from_str(time.as_str(), "%H:%M:%S").expect("parse error").into(),
+            filters: filters,
+            log_type: log_type,
+            log_type_custom: log_type_c,
+            message: log_msg
+        }
+    }
+    
+    fn match_log_type(s: String) -> TEPILogType {
+        match s.as_str() {
+            "NORMAL" => {return TEPILogType::NORMAL;}
+            "WARN" => {return TEPILogType::WARN;}
+            "ERROR" => {return TEPILogType::ERROR;}
+            _ => {return TEPILogType::CUSTOM;}
+        }
     }
 }
 
